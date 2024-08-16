d51e2ae1a5a1a906ef079dfd10542036fcc55181
Rakib <hellorakibhossen@gmail.com>
2024-08-15 21:03:33 -0700

    account profile done

---
+++ index.js
@@ -0,0 +1,6 @@
+let Accountbtn = document.getElementById("Accountbtn");
+let profilebtn = document.getElementById("profilebtn");
+
+Accountbtn.addEventListener("click", () => {
+  profilebtn.classList.toggle("hidden");
+});
